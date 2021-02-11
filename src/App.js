f0cadac6345ec5867663d952d8a9a841d4730708
Meriem BADER <meriembader8@gmail.com>
2021-02-11 11:50:00 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
+import React, { Suspense } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import styled from "styled-components";
 import Header from "./components/Header";
-import Welcome from "./pages/Welcome";
-import Products from "./pages/Products";
-import ProductDetails from "./pages/ProductDetails";
+const Welcome = React.lazy(() => import("./pages/Welcome"));
+const Products = React.lazy(() => import("./pages/Products"));
+const ProductDetails = React.lazy(() => import("./pages/ProductDetails"));
 function App() {
+  const connectedUser = { name: "FakeUser", roles: ["FakeAdmin"] };
   return (
   <>
   <Header></Header>
   <AppFrame className="App">
+  
   <BrowserRouter basename="/">
+  <Suspense fallback={<p>...Loading page please wait</p>}>
+  {connectedUser ? (
   <Switch>
   <Route
   path="/welcome"
@@ -24,16 +29,41 @@ function App() {
   path="/product/:name"
   render={(props) => <ProductDetails {...props} />}
   ></Route>
-  <Route exact render={() => <p>Default rendered page!</p>}></Route>
+  <Route
+  exact
+  render={() => (
+  <p>Default rendered page! Welcome {connectedUser.name}</p>
+  )}
+  ></Route>
   </Switch>
+  ) : (
+  <Switch>
+  <Route
+  path="/welcome"
+  render={(props) => <Welcome {...props} />}
+  ></Route>
+  <Route
+  path="/login"
+  render={(props) => <Welcome {...props} />}
+  ></Route>
+  <Route
+  path="/products"
+  render={(props) => <Products {...props} />}
+  ></Route>
+  <Route
+  path="/product/:name"
+  render={(props) => <ProductDetails {...props} />}
+  ></Route>
+  <Route
+  exact
+  render={() => <p>You are not logged in!</p>}
+  ></Route>
+  
+  </Switch>
+  )}
+  </Suspense>
   </BrowserRouter>
   </AppFrame>
   </>
   );
   }
-  const AppFrame = styled.div`
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-  `;
-  export default App;
